e54fc316e35cb3026d1b3c4447b9ad3afcebbd33
shubhangi831 <shubhangi099kamble@gmail.com>
2024-09-11 18:16:24 +0530

    add new custom-plugin.php file

---
+++ custom-plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name:       Custom Plugin
+ * Plugin URI:        https://example.com
+ * Description:       Customize plugin
+ * Version:           1.0.0
+ * Author:            Shubhangi
+ * Author URI:        https://example.com
+ * Requires PHP:      7.0
+ */
+
+//Metabox event custom post type
